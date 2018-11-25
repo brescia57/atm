39b4140755e61ddaaf213c5858683c292a0d6a59
Charles Hedji <charleshedji@gmail.com>
2018-11-25 11:43:01 -0500

    added withdraw method and functionality

--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -3,5 +3,9 @@ require './lib/atm.rb'
 describe Atm do
   it 'has 1000$ on initialize' do
     expect(subject.funds).to eq 1000
-  end  
+  end
+  it 'funds are reduced at withdraw' do
+    subject.withdraw 50
+    expect(subject.funds).to eq 950
+  end
 end
