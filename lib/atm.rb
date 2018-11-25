1a2547ffad319aad337399f93a6325917a94765b
Charles Hedji <charleshedji@gmail.com>
2018-11-25 11:36:36 -0500

    added class atm and initialize method

---
+++ lib/atm.rb
@@ -0,0 +1,8 @@
+
+class Atm
+  attr_accessor :funds
+  #initialize is a constructor method that will run every time an instance of a class is created
+  def initialize
+    @funds = 1000
+  end
+end
